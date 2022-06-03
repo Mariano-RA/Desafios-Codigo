f301e3c2168ad780a94ebe4721e2ff8d69a982e9
Mariano Rodriguez <rodriguezarce.mariano@gmail.com>
2022-06-03 14:04:20 -0300

    modificado archivo queens attack

--- Queen's Attack II/queen.js
+++ Queen's Attack II/queen.js
@@ -1,37 +1,28 @@
-// Function Description
+function queensAttack() {
+  let n = 5;
+  let nt = 1;
+  let r_q = 3;
+  let c_q = 2;
+  let obstacles = [2, 3];
 
-// Complete the queensAttack function in the editor below.
+  // The first line contains two space-separated integers  and , the length of the board's sides and the number of obstacles.
+  // The next line contains two space-separated integers  and , the queen's row and column position.
+  // Each of the next  lines contains two space-separated integers  and , the row and column position of .
 
-// queensAttack has the following parameters:
-// - int n: the number of rows and columns in the board
-// - nt k: the number of obstacles on the board
-// - int r_q: the row number of the queen's position
-// - int c_q: the column number of the queen's position
-// - int obstacles[k][2]: each element is an array of  integers, the row and column of an obstacle
+  let diagonal = 1;
+  r_q--;
+  c_q--;
+  let obstaculo = false;
+  let tablero = new Array(n).fill(null).map(() => new Array(n).fill(0));
 
-function queensAttack(){
-    let n = 4;
-    let nt = 0;
-    let r_q = 4;
-    let c_q = 4;
-    // let obstacles = [1,2];
+  tablero[obstacles.slice(0, 1) - 1][obstacles.slice(1, 2) - 1] = 4;
+  tablero[r_q][c_q] = 3;
 
-    // The first line contains two space-separated integers  and , the length of the board's sides and the number of obstacles.
-    // The next line contains two space-separated integers  and , the queen's row and column position.
-    // Each of the next  lines contains two space-separated integers  and , the row and column position of .
+  
 
-    r_q--;
-    c_q--;
+  tablero.reverse();
+  console.log(tablero)
 
-    let tablero = new Array(n).fill(null).map(() => new Array(n).fill(0));
-    tablero[r_q][c_q] = 1;
-    
-    console.log(tablero)
-
-    for(let i=0;i <tablero.length; i++){
-        for(let k=0;k < tablero[i].length;k++)
-        tablero[]
-    }
 }
 
-queensAttack()
+queensAttack();
